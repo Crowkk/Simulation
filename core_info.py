e11689d34e7f177c5ba2d037bbd0f98eff79aecf
crowkk <regis.victor.ufrj@gmail.com>
2021-03-18 22:55:27 -0300

    pass

--- core_info.py
+++ core_info.py
@@ -1,7 +1,17 @@
 import numpy as np
 
-class dweller():
-    pass
 
+###INFO about the dweller
+class Dweller:
+    def __init__(self,name,stats):
+        self.name = name
+        if not isinstance(stats,list) or len(stats)!= 1:
+            raise Exception("error")
+        else:
+            self.stats = stats
+
+#INFO about the move
 class move():
     pass
+
+
