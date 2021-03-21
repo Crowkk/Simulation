45aaa529b9f21f511bcec9439a2242ddbf2aff32
crowkk <regis.victor.ufrj@gmail.com>
2021-03-21 19:58:09 -0300

    added some stuff

--- core_info.py
+++ core_info.py
@@ -7,6 +7,10 @@ from copy import *
 #FROM SECONDCLASS DEFINE ALL THE STATUSES BASED ON STATS_LIST AND MODIFIER_LIST
 #CHECK WITHIN SECONDCLASS THE STATUSES EFFECTS
 
+#status does not work
+
+#Poison, Soaked, Burned, Paralyzed, Muted, Cursed
+
 #ADD PROPER DAMAGE STUFF AND TRIGGER EFFECTS AND DIFFERENTIATE THE CATEGORIES
 
 #ADD INTERACTIONS TABLE
@@ -16,6 +20,7 @@ from copy import *
 #ADD MORE DWELLERS
 
 #NATURALITY
+#Maybe create a subclass for each naturality special effect
 
 #ENVIROMENT MODIFIERS
 
@@ -37,27 +42,45 @@ class Dweller:
         #need to have still current_hp, buffs, conditional_effects, external_info
         self.move_set = move_set
         self.streak = 0
+        self.status = ""
+        self.external = None
     @property
     def speed(self):
         return self.stats[4]/(2**self.streak)
 
+    def status_proc(self,proc):
+        if self.status == "":
+            print(self.name," now has ",proc) 
+            self.status = proc
+        print("Already status")
+
+    @property
+    def accuracy(self):
+        return self.stats[2]-0.1*(self.status == "Soaked")
+
+    def check_status(self,stage):
+        if self.status == "":
+            self.stats[0] =  self.stats[0]
+        elif stage == "after life check":
+            if self.status == "Poison":
+                self.stats[0] = self.stats[0] - 100
 
 #INFO about the move
 class Move: #this class will probably have an instance that is the Statuses/Conditions Class
-    def __init__(self,name,stats,kind,element,secondary=None): #and additional info for secondary effects etc
+    def __init__(self,name,stats,kind,element,secondary): #and additional info for secondary effects etc
         self.name = name
         self.stats = stats #damage crit acc falloff area 
         self.kind = kind #physical elemental etc
         self.element = element #fire etc
-        self.secondary = secondary
+        self.secondary = secondary #an array with numbers corresponding to [who_affects, what_affects, how_affects]
 
 Fae_stats = np.array([500,0.05,0.95,0.05,0.5,50,50,50,50,50,50])
 Arson_stats = np.array([500,0.05,0.95,0.05,0.5,50,50,50,50,50,50])
-Flamethrower = Move("Flamethrower",np.array([50,0.05,0.98,4]),2,0)
-Fae_dust = Move("Fae Dust",np.array([50,0.05,0.98,4]),2,0)
+Flamethrower = Move("Flamethrower",np.array([50,0.05,0.98,4]),7,0,np.array([]))
+Fae_dust = Move("Fae Dust",np.array([50,0.05,0.98,4]),9,0,secondary = np.array([0.9,[1],[[],[0]],[[],["Poison"]]],dtype = object))
 Fairy = Dweller("Fae",Fae_stats,1,1,1,1,np.array([Flamethrower,Fae_dust]))
 Arsonist = Dweller("Arson",Arson_stats,1,1,1,1,np.array([Flamethrower,Fae_dust]))
-
+print(len(Flamethrower.secondary))
 #Each turn will go as follows (in the most basic way)
 #firstly the attacker is picked according to a function
 #then it chooses an attack to use
@@ -75,6 +98,19 @@ def who_attacks(Dweller1,Dweller2):
     defender = attacker_choice[1]
     return attacker,defender #outputs who attacks and who defends
 
+def combat_status_checker(D1,D2,stage):
+    D1.check_status(stage)
+    D2.check_status(stage)
+
+def status_effect(secondary,who,attacker,defender):
+    targets = [attacker,defender]
+    if secondary[2][who] == 0: #it's a status
+        
+        targets[who].status_proc(secondary[3][who])        
+    elif secondary[2][who] == 1: #it's buff or debuff
+        targets[who].external += secondary[3][who]
+    return attacker,defender
+            
 def combat(attacker,defender): #routine related to the combat alone, outputs the final damage
     def is_crit(move,attacker): #calculates the critical stuff
         c = move.stats[1]*attacker.stats[3]>=np.random.random() #whether or not a critical landed
@@ -84,23 +120,39 @@ def combat(attacker,defender): #routine related to the combat alone, outputs the
         return 1 #no bonus modifier
 
     def damage(move,attacker,defender): #calculates final damage
-        return move.stats[0]*attacker.stats[5]/defender.stats[6] #simple formula base damage * (attacker_attack)/defender_defense
+        return move.stats[0]*attacker.stats[move.kind]/defender.stats[move.kind] #simple formula base damage * (attacker_attack)/defender_defense
         
     move = np.random.choice(attacker.move_set) #randomly choose an attack
     total_accuracy = attacker.stats[2]*move.stats[2]*(1-defender.stats[1]) #combine all accuracy and evasivenesses involved
+    print(attacker.name," used ",move.name)
     is_hit = np.random.random() <= total_accuracy #does hit
     if not is_hit:
         print(defender.name + " dodged the attack")
-        return 0
-    return is_crit(move,attacker)*damage(move,attacker,defender) #damage
-    
+        return 0,np.array([None]),move #ajeitar aqui
+    if move.kind !=0:
+        if len(move.secondary) != 0:
+            does_proc = np.random.random() 
+            return is_crit(move,attacker)*damage(move,attacker,defender),move.secondary*(does_proc<=move.secondary[0]),move #damage
+        else:
+            return is_crit(move,attacker)*damage(move,attacker,defender),0,move
+    else:
+        return 0,move.secondary,move
 def turn(Dweller1,Dweller2): #general turn
     while Dweller1.stats[0]*Dweller2.stats[0] >0: #both are alive
+        stage = "before"
+        combat_status_checker(Dweller1,Dweller2,stage)
         attacker, defender = who_attacks(Dweller1,Dweller2) #chose the attacker for the turn
-        dmg = combat(attacker,defender) #calculated the damage
+        dmg,secondary,move = combat(attacker,defender) #calculated the damage
         if dmg: #was there any damage?
             defender.stats[0] -= dmg #subtracts from total hp
             print(defender.name, ' was dealt ',str(dmg),' damage')
+        if len(move.secondary) != 0 and secondary[0] != None:
+            for element in secondary[1]:
+                attacker,defender = status_effect(secondary,element,attacker,defender)
+                    
+        if Dweller1.stats[0]*Dweller2.stats[0] >0:
+            stage = "after life check"
+            combat_status_checker(attacker,defender,stage)
         
     #this snipet will change soon, works fine it's just ugly
     if Dweller1.stats[0] <= 0: ##use np.where 
@@ -109,6 +161,4 @@ def turn(Dweller1,Dweller2): #general turn
         print(Dweller2.name, " lost")
 
 
-
-
-
+turn(Arsonist,Fairy)
